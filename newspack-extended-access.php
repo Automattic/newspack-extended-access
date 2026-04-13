33f3dccbb40ca7c1292ddd7ae92994c7df1d086d
Adam Cassis <adam.cassis@automattic.com>
2026-04-13 12:45:21 +0200

    chore(release): 1.1.0

--- newspack-extended-access.php
+++ newspack-extended-access.php
@@ -6,7 +6,7 @@
  * Author:          Automattic
  * Text Domain:     newspack-extended-access
  * Domain Path:     /languages
- * Version:         1.0.1
+ * Version:         1.1.0
  *
  * @package Newspack\ExtendedAccess
  */
